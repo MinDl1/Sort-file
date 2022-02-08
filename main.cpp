ff88cd9d732aa713f48219263f10034d788d2a7c
MinDl <88254906+MinDl1@users.noreply.github.com>
2022-02-08 23:16:45 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,91 @@
+//Made by MinDl
+//
+//8.2.2022
+//
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	ifstream file_in("file.txt");
+	if (!file_in.is_open()) {
+		cout << "Error: File can't open";
+		return 0;
+	}
+	vector<string> text;
+	vector<string> lastname_firstname;
+	string buff;
+
+	cout << "Unsorted table: \n";
+	while (getline(file_in, buff)) {
+		text.push_back(buff);
+	}
+	file_in.close();
+
+	for (string n : text) {
+		cout << n << endl;
+	}
+
+	for (int j = 0; j < text.size(); j++){
+		string str = text[j];
+
+		string firstname = str;
+		for (int i = 0, begin_ = 0, end_ = 0, spaces = 0; i < firstname.size(); i++) {
+			if (firstname[i] == ' ' && spaces == 0) {
+				begin_ = i + 1;
+				spaces++;
+			}
+			else if (firstname[i] == ' ' && spaces == 1) {
+				end_ = i;
+				firstname = firstname.substr(begin_, end_ - begin_);
+				break;
+			}
+		}
+
+		string lastname = str;
+		for (int i = 0, begin_ = 0, end_ = 0, spaces = 0; i < lastname.size(); i++) {
+			if (lastname[i] == ' ') {
+				spaces++;
+			}
+			if (spaces == 2 && begin_ == 0) {
+				begin_ = i + 1;
+			}
+			else if (spaces == 3 && end_ == 0) {
+				end_ = i;
+				lastname = lastname.substr(begin_, end_ - begin_);
+				break;
+			}
+		}
+
+		lastname_firstname.push_back(lastname + ' ' + firstname);
+		text[j] = lastname_firstname[j] + " " + text[j];
+	}
+	lastname_firstname.clear();
+	sort(text.begin(), text.end());
+
+	for (int i = 0; i < text.size(); i++) {
+		for (int j = 0, spaces = 0; j < text[i].size(); j++) {
+			if (text[i][j] == ' ') {
+				spaces++;
+			}
+			if (spaces == 2) {
+				text[i].replace(0,j+2,to_string(i+1));
+				break;
+			}
+		}
+	}
+	cout << "\nSorted table: \n";
+
+	ofstream file_out("file.txt", ios_base::out | ios_base::trunc);
+
+	for (string it : text) {
+		cout << it << endl;
+		file_out << it << endl;
+	}
+	file_out.close();
+	text.clear();
+}
